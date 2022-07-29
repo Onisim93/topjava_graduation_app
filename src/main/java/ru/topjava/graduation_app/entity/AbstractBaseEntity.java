61f58389aeb810af2f29cb7f6a424cb13302cf05
Onisim Albarov <Onisim93@gmail.com>
2022-07-29 18:02:32 +0400

    add abstract classes for entity

--- src/main/java/ru/topjava/graduation_app/entity/AbstractBaseEntity.java
+++ src/main/java/ru/topjava/graduation_app/entity/AbstractBaseEntity.java
@@ -1,22 +1,57 @@
 package ru.topjava.graduation_app.entity;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.Hibernate;
 import org.springframework.data.domain.Persistable;
+import org.springframework.util.Assert;
 
 import javax.persistence.*;
 
 @MappedSuperclass
-public abstract class AbstractBaseEntity implements Persistable<Long> {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    protected Long id;
+@Getter
+@Setter
+@Access(AccessType.FIELD)
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class AbstractBaseEntity implements Persistable<Integer> {
+    public static final int START_SEQ = 100000;
 
-    @Override
-    public Long getId() {
+    @Id
+    @SequenceGenerator(name = "global_seq", sequenceName = "global_seq", allocationSize = 1, initialValue = START_SEQ)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "global_seq")
+    @JsonSerialize
+    protected Integer id;
+
+    public int id() {
+        Assert.notNull(id, "Entity must have id");
         return id;
     }
 
+    @JsonIgnore
     @Override
     public boolean isNew() {
         return this.id == null;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || !getClass().equals(Hibernate.getClass(o))) {
+            return false;
+        }
+        AbstractBaseEntity that = (AbstractBaseEntity) o;
+        return id != null && id.equals(that.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return id == null ? 0 : id;
     }
 }
---
+++ src/main/java/ru/topjava/graduation_app/entity/AbstractNamedEntity.java
@@ -0,0 +1,26 @@
+package ru.topjava.graduation_app.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.MappedSuperclass;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@MappedSuperclass
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public abstract class AbstractNamedEntity extends AbstractBaseEntity{
+    @NotBlank
+    @Size(min = 2, max = 128)
+    protected String name;
+
+    @Override
+    public String toString() {
+        return super.toString() + '(' + name + ')';
+    }
+}
